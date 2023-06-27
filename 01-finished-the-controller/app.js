6e238f4081adca48fffd6ea2714b2ecb097ddb36
krushnasharma05 <129397615+krushnasharma05@users.noreply.github.com>
2023-06-27 14:19:44 +0530

    dynamic_routes1

--- 01-finished-the-controller/app.js
+++ 01-finished-the-controller/app.js
@@ -1,24 +1,24 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const errorController = require('./controllers/error');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Admin routes
-const adminRoutes = require('./routes/admin');
 app.use('/admin', adminRoutes);
-
-// Shop routes
-const shopRoutes = require('./routes/shop');
 app.use(shopRoutes);
 
-// 404 error handling
 app.use(errorController.get404);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+app.listen(3000);
--- 01-finished-the-controller/controllers/error.js
+++ 01-finished-the-controller/controllers/error.js
@@ -1,3 +1,3 @@
 exports.get404 = (req, res, next) => {
-  res.status(404).send('<h1>Page Not Found</h1>');
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };

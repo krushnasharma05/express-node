f4db9013aef7cd2277f20568bacf78f358963895
krushnasharma05 <129397615+krushnasharma05@users.noreply.github.com>
2023-06-28 14:19:02 +0530

    starting sql

---
+++ util/database.js
@@ -0,0 +1,9 @@
+const mysql= require('mysql2');
+const pool=  mysql.createPool({
+     host: 'localhost',
+     user: 'root',
+     database: 'node-complete',
+     password: 'Krushna@123'
+});   
+
+module.exports= pool.promise();

dc655d289bffbce97189cbd9279a7efdb1e7d645
krushnasharma05 <129397615+krushnasharma05@users.noreply.github.com>
2023-06-28 23:05:12 +0530

    starting sequelize

--- util/database.js
+++ util/database.js
@@ -1,9 +1,7 @@
-const mysql= require('mysql2');
-const pool=  mysql.createPool({
-     host: 'localhost',
-     user: 'root',
-     database: 'node-complete',
-     password: 'Krushna@123'
-});   
+const Sequelize = require('sequelize');
 
-module.exports= pool.promise();
+const sequelize = new Sequelize('node-complete','root','Krushna@123',{
+     dialect: 'mysql', 
+     host:'localhost'});
+
+module.exports = sequelize;

14f9850d2042038b5ac16c2be24b7635623ef12a
krushnasharma05 <129397615+krushnasharma05@users.noreply.github.com>
2023-06-29 19:04:01 +0530

    full stack expense app

--- backend/app.js
+++ backend/app.js
@@ -1,84 +1,60 @@
 const express = require("express");
 const cors = require('cors');
-const user = require('./database');
 
-const path = require('path');
-const filePath = path.join(__dirname, '..', 'view.html');
+const expence = require('./database');
 
-const sequelize = require('sequelize');
-const sq = new sequelize('node', 'root', 'Krushna@123', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
 
-const app = express();
+const app =express();
+
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/saveUser', async (req, res, next) => {
-  const { name, phone, email } = req.body;
-  try {
-    const data = await user.create({ name, phone, email });
-    res.json(data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
 
-app.get('/getUser', async (req, res) => {
-  try {
-    const users = await user.findAll();
-    res.json(users);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
+try{
+  app.post('/saveProduct',async (req,res,next)=>{
 
-app.patch('/updateUser/:id', async (req, res) => {
-  const { id } = req.params;
-  const { name, phone, email } = req.body;
-  try {
-    const updateUser = await user.findByPk(id);
-    if (!updateUser) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    updateUser.name = name;
-    updateUser.phone = phone;
-    updateUser.email = email;
-    await updateUser.save();
-    res.json({ message: 'User updated successfully' });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
+    const name = req.body.name;
+    const description = req.body.description;
+    const price = req.body.price;
+    const quantity = req.body.quantity;
+    let data = await expence.create({name: name,description: description,price: price,quantity: quantity});
+    res.send('success');
+  });
+}
+catch(err){console.log(err)};
 
-app.delete('/deleteUser/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const deletedUser = await user.destroy({ where: { id } });
-    if (deletedUser === 0) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    res.json({ message: 'User deleted successfully' });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
 
-user.sync()
-  .then(() => {
-    app.get('/', (req, res) => {
-      res.sendFile(filePath);
-    });
+try{
+  app.get('/getProducts',async (req,res)=>{
+    const expences = await expence.findAll();
+    res.json(expences);
+  });
+}
+catch(err){console.log(err)};
+// app.patch('/updateUser/:id',async (req,res)=>{
+//   let Uid = req.params.id;
 
-    app.listen(3000, () => {
-      console.log('Server is running on port 3000');
-    });
-  })
+//   let updateUser = await user.findByPk(Uid);
+//     updateUser.name = req.body.name;
+//     updateUser.phone = req.body.phone;
+//     updateUser.email = req.body.email;
+//     await updateUser.save();
+//     res.json(1);
+
+// });
+
+try{
+app.delete('/deleteExpence/:id', async (req,res)=>{
+  let Uid = req.params.id;
+  await expence.destroy({ where: { id: Uid } });
+  res.send('success');
+});}
+
+catch(err){console.log(err)};
+
+
+expence
+  .sync()
+  .then(result => app.listen(3000))
   .catch(err => console.log(err));
--- backend/database.js
+++ backend/database.js
@@ -1,29 +1,33 @@
 const sequelize = require('sequelize');
 
 const sq = new sequelize('node', 'root', 'Krushna@123', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+    dialect: 'mysql',
+    host: 'localhost'
+  });
 
-const user = sq.define('user', {
-  id: {
-    type: sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: {
-    type: sequelize.STRING,
-    allowNull: false
-  },
-  phone: {
-    type: sequelize.BIGINT,
-    allowNull: false
-  },
-  email: {
-    type: sequelize.STRING,
-    allowNull: false
-  }
-});
 
-module.exports = user;
+const user = sq.define('Expence', {
+    id: {
+      type: sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+        type :sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+      type: sequelize.TEXT,
+      allowNull: false
+    },
+    price: {
+      type: sequelize.INTEGER,
+      allowNull: false
+    },
+    quantity: {
+      type: sequelize.INTEGER,
+      allowNull: false
+    }
+  });
+  module.exports = user;
